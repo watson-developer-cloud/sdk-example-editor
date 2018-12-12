0aedd1b4862eac8ceafa4a19bf6042467e061a41
German Attanasio <germanattanasio@gmail.com>
2018-12-12 11:38:18 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: [
+    'core-js/modules/es6.promise',
+    'core-js/modules/es6.array.iterator',
+    './src/index.js',
+  ],
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
