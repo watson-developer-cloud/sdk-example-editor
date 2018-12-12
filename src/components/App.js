90e0c01a8a8c8fbaa9dfa96d9f0ac57bb3886933
Logan Patino <loganpatino10@gmail.com>
2018-12-12 13:17:24 -0500

    feat: Add carbon react components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Button } from 'carbon-components-react';
 
 import '../../node_modules/carbon-components/css/carbon-components.css';
 
 export class App extends Component {
   render() {
-    return <p>Hi, I've been packed, and transpiled</p>
+    return <Button>what a cool button</Button>;
   }
 }
