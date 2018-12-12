74db54b43f2b54df00563ee500a93729ee2bd8cf
German Attanasio <germanattanasio@gmail.com>
2018-12-12 14:36:02 -0500

    fix: add prettier and eslint

--- src/components/InputContainer.js
+++ src/components/InputContainer.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
-import { FileUploader, TextInput } from 'carbon-components-react';
+import React, {Component} from 'react';
+import {FileUploader, TextInput} from 'carbon-components-react';
 import '../../node_modules/carbon-components/css/carbon-components.css';
 import './styles.css';
 
 export class InputContainer extends Component {
   render() {
     return (
-      <div className='input-container'>
-        <FileUploader buttonLabel='Select Swagger file' filenameStatus='edit' />
-        <TextInput className='url-input' id='url-input' labelText='Add URL for Swagger file'/>
+      <div className="input-container">
+        <FileUploader buttonLabel="Select Swagger file" filenameStatus="edit" />
+        <TextInput
+          className="url-input"
+          id="url-input"
+          labelText="Add URL for Swagger file"
+        />
       </div>
     );
     // has file upload button and text input vertically
