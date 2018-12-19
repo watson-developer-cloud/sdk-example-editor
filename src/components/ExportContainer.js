9dfec6dd7e483983152272e76692213438dd64e8
Logan Patino <loganpatino10@gmail.com>
2018-12-19 09:58:42 -0500

    feat(ExportContainer): Create zip file with full swagger and JSON examples

--- src/components/ExportContainer.js
+++ src/components/ExportContainer.js
@@ -2,13 +2,58 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Button} from 'carbon-components-react';
 import FileSaver from 'file-saver';
-import {getSwagger} from '../selectors/index';
-import {buildSwaggerFile} from '../utils/utils';
+import JSZip from 'jszip';
+import {getSwagger, getSelectedLanguage} from '../selectors/index';
 import './ButtonContainer.css';
 
 class ExportContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.buildOutputFile = this.buildOutputFile.bind(this);
+  }
+
+  buildOutputFile(swagger, selectedLanguage) {
+    const zip = new JSZip();
+    zip.file(
+      'new-swagger.json',
+      new Blob([JSON.stringify(swagger, null, 2)], {type: 'application/json'}),
+    );
+
+    const exampleFolder = zip.folder('examples');
+    Object.entries(swagger.paths).forEach(([_, pathInfo]) => {
+      Object.entries(pathInfo).forEach(([_, methodInfo]) => {
+        // this is something we don't want, like a parameters array
+        if (Array.isArray(methodInfo)) {
+          return;
+        }
+
+        const sdkExamples = methodInfo['x-sdk-operations'];
+        if (
+          sdkExamples &&
+          sdkExamples['request-examples'] &&
+          sdkExamples['request-examples'][selectedLanguage]
+        ) {
+          let blobArray = [];
+          sdkExamples['request-examples'][selectedLanguage].forEach(example => {
+            blobArray.push(
+              JSON.stringify(example['example'][0]['source'], null, 2),
+            );
+          });
+          exampleFolder.file(
+            `${methodInfo.operationId}.json`,
+            new Blob(blobArray, {type: 'application/json'}),
+          );
+        }
+      });
+    });
+
+    zip
+      .generateAsync({type: 'blob'})
+      .then(blob => FileSaver.saveAs(blob, 'examples.zip'));
+  }
+
   render() {
-    const {swagger} = this.props;
+    const {selectedLanguage, swagger} = this.props;
 
     return (
       <div className="container">
@@ -16,7 +61,7 @@ class ExportContainer extends Component {
           className="button"
           disabled={swagger == null}
           onClick={() => {
-            FileSaver.saveAs(buildSwaggerFile(swagger));
+            this.buildOutputFile(swagger, selectedLanguage);
           }}
           type="submit"
         >
@@ -28,6 +73,7 @@ class ExportContainer extends Component {
 }
 
 const mapStateToProps = state => ({
+  selectedLanguage: getSelectedLanguage(state),
   swagger: getSwagger(state),
 });
 
