4319879da0190a62aec3e2c8f06b89fea72aa293
German Attanasio <germanattanasio@gmail.com>
2020-04-30 12:30:34 -0400

    Updates

---
+++ precache-manifest.bbf1af7d0707fa61dc5ad82fea675571.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64fce0a67c7b2d8fcc4e8b8cda8e2110",
+    "url": "/sdk-example-editor/index.html"
+  },
+  {
+    "revision": "146da943ad6712bfd8ae",
+    "url": "/sdk-example-editor/static/css/main.6fb2686e.chunk.css"
+  },
+  {
+    "revision": "99b34d334bf88c78c538",
+    "url": "/sdk-example-editor/static/js/2.0610ee3d.chunk.js"
+  },
+  {
+    "revision": "7ce8fada3851689ced6ece7f02cca482",
+    "url": "/sdk-example-editor/static/js/2.0610ee3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "146da943ad6712bfd8ae",
+    "url": "/sdk-example-editor/static/js/main.729caebf.chunk.js"
+  },
+  {
+    "revision": "f43bdd9e96ca18e44a4c",
+    "url": "/sdk-example-editor/static/js/runtime-main.1296ef5e.js"
+  }
+]);
